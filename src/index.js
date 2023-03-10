aacdd60ac3841573a9cf7872a1a8f455bed2474a
adilett02 <adiletturdakunov2002@gmail.com>
2023-03-10 19:06:31 +0600

    init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC3HcrtjPzl2SjgWT8sp3dsmFTBWtZqCxA',
+  authDomain: 'poker-5af61.firebaseapp.com',
+  projectId: 'poker-5af61',
+  storageBucket: 'poker-5af61.appspot.com',
+  messagingSenderId: '77485420186',
+  appId: '1:77485420186:web:b798299a964ac8e4233923',
+  measurementId: 'G-EH1CBW9V1R',
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// request.time < timestamp.date(2023, 4, 3)
